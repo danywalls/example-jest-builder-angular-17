99ffd1f158ce65f9ce37be0cc71a714cb5f4241a
Dany Paredes <danywalls4@gmail.com>
2023-11-25 19:24:38 +0100

    setup jest builder
    setup jest builder

---
+++ src/app/components/toaster/toaster.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-toaster',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './toaster.component.html',
+  styleUrl: './toaster.component.scss'
+})
+export class ToasterComponent {
+
+}
